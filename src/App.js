bfa04f533e344e9e0708f24940ae7d49b63636f1
D'Riski Maulana <68141563+driskimaulana@users.noreply.github.com>
2022-04-18 10:32:46 +0700

    final commit for mobile device web

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Card from './Components/Card';
+import cardData from './Data/cardData';
 
 function App() {
+  console.log(cardData);
+  const getCardData = cardData.map(data => {
+    return <Card 
+              key={data.id}
+              data={data}
+            />
+  })
   return (
     <div className="container">
       <Navbar />
-      {/* <Hero /> */}
-      <Card />
+      <Hero />
+      <section className="card--list">
+        {getCardData}
+      </section>
     </div>
   );
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,23 +1,33 @@
 import React from "react";
 import Katie from "../Assets/images/katie-zaferes.png"
 import Stars from "../Assets/images/star.png"
+import cardData from "../Data/cardData";
 
-export default function Card()
+export default function Card(props)
 {
+    console.log(props)
+
+    let badgeText;
+    if(props.data.openSpots === 0){
+        badgeText = "SOLD OUT";
+    }else if(props.data.location === "Online"){
+        badgeText = "ONLINE"
+    }
+
+    const badge = <div className="card--badge">{badgeText}</div>
+
     return (
         <div className="card">
-            
-            <img className="card--image" src={Katie} />
-            {/* <div className="card--status">
-                <p>SOLD</p>
-            </div> */}
+
+            {!badgeText ? "" : badge}
+            <img className="card--image" src={ require(`../Assets/images/${props.data.coverImg}`)} />
             <div className="card--rating">
                 <img src={Stars} />
-                <p className="card--rating-primary">5.0</p>
-                <p className="card--rating-secondary">(6) · USA</p>
+                <p className="card--rating-primary">{props.data.stats.rating}</p>
+                <p className="card--rating-secondary">({props.data.stats.reviewCount}) · {props.data.location}</p>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p><span className="card--price">From $136</span> / person</p>
+            <p>{props.data.title}</p>
+            <p><span className="card--price">From ${props.data.price}</span> / person</p>
         </div>
     );
 }
